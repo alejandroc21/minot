f7da73eaeb6ec55d24a43b62a145d7b7dedc81b6
alejandroc21 <alextocar.act@gmail.com>
2025-06-22 17:57:57 -0500

    feature/folder-ui

---
+++ minot-ui/src/app/notes/components/note-grid/note-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './note-grid.component.html',
+  styleUrl: './note-grid.component.css'
+})
+export class NoteGridComponent {
+
+}
--- minot-ui/src/app/notes/components/note/note.component.ts
+++ minot-ui/src/app/notes/components/note/note.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { NoteGridComponent } from "../note-grid/note-grid.component";
+import { NoteListComponent } from "../note-list/note-list.component";
 
 @Component({
   selector: 'app-note',
   standalone: true,
-  imports: [],
+  imports: [NoteGridComponent, NoteListComponent],
   templateUrl: './note.component.html',
   styleUrl: './note.component.css'
 })
 export default class NoteComponent {
+  viewGrid = true;
 
+  showGrid(){
+    this.viewGrid = true;
+  }
+  showList(){
+    this.viewGrid = false;
+  }
 }
